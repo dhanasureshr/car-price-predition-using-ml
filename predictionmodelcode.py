c5ca2e765b1d7f3cae40098de51fcf7abec52e67
dhanasuresh <dhanasoftwares@email.com>
2021-11-27 17:48:43 +0530

    Checking correlation between features

--- predictionmodelcode.py
+++ predictionmodelcode.py
@@ -7,4 +7,23 @@ import seaborn as sns
 
 df = pd.read_csv('Car data.csv')
 
-print(df.sample(10))
+# removing car name
+final_dataset = df[['Year','Selling_Price','Present_Price','Kms_Driven','Fuel_Type','Seller_Type','Transmission','Owner']]
+
+# adding current year columns
+final_dataset['Current Year']=2021
+# Calculate the number of years (no_Years), how many years ago owner bought a car
+final_dataset['No_Year'] = final_dataset['Current Year'] - final_dataset['Year']
+
+# droping the temperory features
+final_dataset.drop(['Year'],axis=1,inplace=True)
+final_dataset = final_dataset.drop(['Current Year'],axis=1)
+
+# Checking the correleation between features
+corrmat = df.corr()
+top_corr_features = corrmat.index
+plt.figure(figsize=(10,10))
+# plot head map
+g = sns.heatmap(df[top_corr_features].corr(),annot=True,cmap="RdYlGn")
+plt.savefig("Feature_correlation.png")
+
