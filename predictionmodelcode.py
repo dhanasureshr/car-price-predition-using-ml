2b184d54c4556555c06c2cfbfb5e5f7ff48c91c3
dhanasuresh <dhanasoftwares@email.com>
2021-11-27 17:09:47 +0530

    imported dataset

--- predictionmodelcode.py
+++ predictionmodelcode.py
@@ -1,3 +1,10 @@
 import pandas as pd
 import numpy as np
-import mataplotlib.pyplot as plt
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+# loading the data set
+
+df = pd.read_csv('Car data.csv')
+
+print(df.sample(10))
