9606038681acd40bc1bcecbdeeab65f40e121834
dhanasuresh <dhanasoftwares@email.com>
2021-11-28 09:03:51 +0530

    testing the model

--- main.py
+++ main.py
@@ -3,14 +3,28 @@
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
+# Building Flask API
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+from flask import Flask,request,jsonify
+import numpy as np
+import  pickle
 
+
+model = pickle.load(open('rf_model.pkl', 'rb'))
+app = Flask(__name__)
+@app.route('/')
+def index():
+    return "Hello world"
+
+@app.route('/predict',methods=['POST'])
+def predict():
+    Fuel_Type_Diesel=0
+    if request.method == 'POST':
+        Year = int(request.form.get('Year'))
+        Present_Price=float(request.form.get('Present_Price'))
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    app.run(debug=True)
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
--- predictionmodelcode.py
+++ predictionmodelcode.py
@@ -1,3 +1,5 @@
+# Builting a Machine Learning model
+
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
