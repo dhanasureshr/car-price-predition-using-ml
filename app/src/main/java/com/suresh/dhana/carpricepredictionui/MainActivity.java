570fc419c55d0a982df667183d8e02271c6401d2
dhana suresh <dhanasuresh0002@gmail.com>
2022-03-14 19:13:27 +0530

    splash screen

--- app/src/main/java/com/suresh/dhana/carpricepredictionui/MainActivity.java
+++ app/src/main/java/com/suresh/dhana/carpricepredictionui/MainActivity.java
@@ -41,6 +41,10 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		///==============================toolbar icon setup=============================
+
+		///====================================end======================================
+
 
 		//===============================================================================
 		//===============================================================================
